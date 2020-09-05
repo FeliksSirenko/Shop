87bc2dc686d2b96d419cb5c5211cf0048d984c32
DGMary <mblnkinakk@gmail.com>
2020-09-05 15:38:39 +0300

    V-for was added for products list

--- Shop/client/src/App.vue
+++ Shop/client/src/App.vue
@@ -17,6 +17,20 @@ export default {
 </script>
 
 <style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+body {
+  min-width: 320px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+  line-height: 1.4;
+  color: #000;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,7 +39,7 @@ export default {
   margin-top: 60px;
 }
 .container {
-  width: 1200px;
+  width: 1100px;
   max-width: 100%;
   padding: 0 20px;
   margin: 0 auto;
@@ -33,5 +47,33 @@ export default {
 .title {
   text-align: center;
   margin: 20px 0 40px;
+}
+.ic-marker {
+  display: inline-flex;
+  width: 18px;
+  height: 18px;
+  background: url(./assets/marker.svg);
+  margin-right: 10px;
+}
+.btn {
+  width: 100%;
+  line-height: 16px;
+  white-space: nowrap;
+  cursor: pointer;
+  display: inline-flex;
+  border-radius: 5px;
+  font-weight: 400;
+  -webkit-appearance: none;
+  align-items: center;
+  justify-content: center;
+  transition: all .2s linear;
+  width: auto;
+  padding: 10px 20px;
+}
+.btn-default {
+  font-size: 18px;
+  border-color: #79be00;
+  background-color: #79be00;
+  color: #ffffff;
 }
 </style>
--- Shop/client/src/components/Catalog.vue
+++ Shop/client/src/components/Catalog.vue
@@ -4,31 +4,38 @@
       <div class="container">
         <h1 class="title">{{ title }}</h1> 
         <ul class="product-list">
-          <li class="product-item">
+          <li class="product-item" v-for="(product, index) in products" :key="index">
             <div class="product-img">
               <a href="">
-                <img :src=product[0].photoData.seoLinkF alt="Product">            
+                <img :src=product.photoData.seoLinkF alt="Product">            
               </a>
             </div>
-            <div class="product-info">
-              <a href="#" class="product-name">{{product[0].markName}}</a>
-              <div class="price-holder">
-                <span class="price">{{product[0].USD}}</span>
-                <span class="price-main">{{product[0].UAH}}</span>
-              </div>
+            <div class="product-info-holder">
+              <a href="#" class="product-name">{{product.markName}}</a> 
+              <div class="product-lacation">
+                <i class="ic-marker"></i>
+                <span>{{product.locationCityName}}</span>
+              </div>             
               <div class="product-info">
                 <ul>
-                  <li><i class=""></i><span>{{product[0].autoData.race}}</span></li>
-                  <li><i class=""></i><span>{{product[0].locationCityName}}</span></li>
-                  <li><i class=""></i><span>{{product[0].autoData.fuelName}}</span></li>
-                  <li><i class=""></i><span>{{product[0].autoData.gearboxName}}</span></li>
+                  <li><span>{{product.autoData.race}}</span></li>
+                  <li><span>{{product.autoData.fuelName}}</span></li>
+                  <li><span>{{product.autoData.gearboxName}}</span></li>
+                  <li><span>{{product.autoData.year}}</span></li>
                 </ul>
                 <div class="description-block">
-                  <span class="description">{{product[0].autoData.description}}</span>
+                  <span class="description">{{product.autoData.description}}</span>
                 </div>
               </div>
+              <div class="price-holder">
+                <span class="price">{{product.USD}}$</span>
+                <span class="price-main">{{product.UAH}} грн</span>
+              </div> 
+              <div class="btn-holder">
+                <div class="btn btn-default">Перезвонить</div>
+              </div>                           
             </div>
-          </li>
+          </li>         
         </ul>
       </div> 
       <div>{{ info }}</div>
@@ -49,7 +56,7 @@ export default {
     title: String
   },
   data: () => ({
-    product: [{
+    products: [{
       locationCityName: "Днепропетровск",                                                   // Город
       auctionPossible: false,                                                               // Торг 
       addDate: "2017-04-24 23:02:06",                                                       // Дата и время добавления 
@@ -96,18 +103,63 @@ export default {
         linkToCatalog: "/city/dnepropetrovsk/",
         stateId: 11                                                                         // id Города
       },  
+    } ,{
+      locationCityName: "Харьков",                                                   // Город
+      auctionPossible: false,                                                               // Торг 
+      addDate: "2017-04-24 23:02:06",                                                       // Дата и время добавления 
+      updateDate: "2017-04-24 23:02:06",                                                    // Дата и время обновления 
+      expireDate: "2017-07-24 23:02:06",                                                    // Дата и время истечения
+      USD: 33500,                                                                           // Стоимость в доларах США
+      UAH: 880380,                                                                          // Стоимость в гривнах
+      levelData: {
+        level: 10,                                                                          // Уровень топа
+        label: 3,                                                                           // Метка
+        hotType: "срочно",                                                                  // Метка "TOP" или "Срочно"
+        expireDate: "2017-05-04 23:02:07"                                                   // Дата и время истечения метки "тор" "Срочно"
+      },
+      autoData: {                                                                           // Характеристики машины
+        description: "Итак. Пришло время в срочном порядке продать свой любимый автомобиль.", // Описание
+        version: "",
+        year: 2019,                                                                         // Год выпуска
+        autoId: 19050985,                                                                   // id объявления
+        statusId: 0,                                                                        //  - 
+        withVideo: false,                                                                   // Наличие видео                               
+        race: "10 тыс. км",                                                                 // Пробег
+        raceInt: 10,                                                                        // - 
+        fuelName: "Дизтопливо 2.0 л",                                                                 // Тип топлива
+        gearboxName: "Механическая",                                                             // Тип привода
+        isSold: false,                                                                      // Этот параметр показывает или машына продана или нет
+        categoryId: 1,                                                                      // Тип транспорта                                                                         
+      },
+      markName: "Ford Ranger 2019",                                                                    // Марка автомобиля
+      markId: 85,                                                                           // id марки автомобиля
+      modelName: "XC90",                                                                    // Модель автомобиля
+      modelId: 824,                                                                         // id Модели автомобиля
+      photoData: {                                                                          // Фотографии
+        count: 15,                                                                          // Количество фотографий
+        seoLinkM: "https://cdn.riastatic.com/photosnew/auto/photo/volvo_xc90__181949196m.jpg",
+        seoLinkSX: "https://cdn.riastatic.com/photosnew/auto/photo/volvo_xc90__181949196sx.jpg",
+        seoLinkB: "https://cdn.riastatic.com/photosnew/auto/photo/volvo_xc90__181949196b.jpg",
+        seoLinkF: "https://cdn.riastatic.com/photosnew/auto/photo/volvo_xc90__181949196f.jpg"
+      },
+      linkToView: "/auto_volvo_xc90_19050985.html",                                         // Линк на объявление
+      title: "Volvo XC90",                                                                  // Название
+      stateData: {                                     
+        name: "Днепропетровск",                                                             // Город
+        regionName: "Днепропетровская",                                                     // Область
+        linkToCatalog: "/city/dnepropetrovsk/",
+        stateId: 11                                                                         // id Города
+      },  
     }], 
     info: null,
-    errorMsg: ""
+    errorMsg: "",
+    loading: true,
   }),
   mounted() {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(response => (this.info = response))
-      .catch(error => {
-        this.errorMsg = error
-        this.info = "Error"
-      });
+      .catch(error => console.log(error)).finally(() => (this.loading = false));
   }
 }
 </script>
@@ -117,6 +169,11 @@ export default {
 
 .product-item {
   display: flex;
+  padding-bottom: 20px;
+}
+.product-item:not(:last-of-type) {
+  border-bottom: 4px solid #f0f2fa;
+  margin-bottom: 20px;
 }
 .product-img {
   width: 300px;
@@ -129,7 +186,73 @@ img {
   max-width: 100%;
   max-height: 100%;
 }
+.product-info-holder {
+  width: calc(100% - 320px);
+}
 .product-list {
   text-align: left;
 }
+.product-name {
+  font-size: 20px;
+  margin: 0 0 10px;
+  color: #256799;
+  text-decoration: none;
+  display: inline-flex;
+  border-bottom: 1px solid transparent;
+  text-decoration-skip-ink: none;
+  transition: border-bottom-color .2s;
+}
+.product-name:hover {
+  text-decoration: none;
+  border-bottom-color: #256799;
+}
+.price-holder {
+  font-size: 18px;
+  margin-bottom: 10px;
+  line-height: 20px;
+}
+.price {
+  display: inline-flex;
+  margin-right: 20px;
+  font-size: 16px;
+  line-height: 20px;
+  color: #666;
+}
+.product-info ul {
+  display: flex;
+  flex-wrap: wrap; 
+  padding: 0;
+  margin: 0;
+}
+.product-info li {
+  width: 50%;
+  padding: 0 10px 10px 16px;
+  max-width: 300px;
+  list-style: none;
+  display: flex;
+  position: relative;
+}
+.product-info {
+  max-width: 500px;
+}
+.product-info li:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  background: #256799;
+  width: 6px;
+  height: 6px;
+  top: 6px;
+}
+.description-block {
+  margin: 10px 0;
+}
+.product-lacation {
+  margin: 0 0 10px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  line-height: 18px;
+}
+
 </style>
